7d80c6823da69f83a1d75a96b7f9aa5879e68c48
Glenn <glennchiang24@gmail.com>
2024-03-07 13:29:11 +0800

    Extract only relevant fields from api for posts and comments

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -10,6 +10,8 @@ REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
 API_URL = 'https://oauth.reddit.com'
 
 # Request for auth token and attach it to request headers
+
+
 def authenticate_session(session: requests.Session, client_id: str, client_secret: str) -> requests.Session:
     basic_auth = HTTPBasicAuth(username=client_id, password=client_secret)
     payload = {'grant_type': 'client_credentials'}
@@ -31,7 +33,7 @@ def authenticate_session(session: requests.Session, client_id: str, client_secre
     return session
 
 
-def get_subreddit_posts(session: requests.Session, subreddit_name: str, limit: int, after: str = None) -> tuple[list, str]:
+def get_posts(session: requests.Session, subreddit_name: str, limit: int, after: str = None) -> list[dict]:
     try:
         response = session.get(
             f'{API_URL}/r/{subreddit_name}/top', params={'limit': limit, 'after': after})
@@ -40,22 +42,35 @@ def get_subreddit_posts(session: requests.Session, subreddit_name: str, limit: i
         raise RequestException(f'Error getting posts from subreddit: {error}')
 
     response_data = response.json()['data']
-    posts = [post['data'] for post in response_data['children']]
-    last_post_id: str = response_data['after']
-    return posts, last_post_id
+    posts = [
+        {
+            'id': post['data']['id'],
+            'title': post['data']['title'],
+            'url': post['data']['url']
+        } for post in response_data['children']
+    ]
+    return posts
 
 
-def get_comments_for_post(session: requests.Session, subreddit_name: str, post_id: str, comment_count: int):
+def get_comments(session: requests.Session, subreddit_name: str, post_id: str, comment_count: int):
     try:
-        response = session.get(f'{API_URL}/r/{subreddit_name}/comments/{post_id}', params={'limit': comment_count})
+        response = session.get(
+            f'{API_URL}/r/{subreddit_name}/comments/{post_id}', params={'limit': comment_count})
         response.raise_for_status()
     except RequestException as error:
         raise RequestException(f'Error getting comments for post: {error}')
 
-    comments = [comment['data'] for comment in response.json()[1]['data']['children']][:-1]
+    comments = [
+        {
+            'body': comment['data']['body'],
+            'permalink': comment['data']['permalink']
+        } for comment in response.json()[1]['data']['children'][:-1]
+    ]
+                
     return comments
 
-def main():
+
+def get_subreddit_thread(subreddit_name: str, num_posts_required: int, comments_per_post: int):
     session = requests.Session()
     try:
         session = authenticate_session(
@@ -63,14 +78,10 @@ def main():
     except RequestException as error:
         raise RequestException(
             f'Error authenticating with reddit API: {error}')
-    
-    target_subreddit = 'AskReddit'
-    num_posts_required = 1
-    # How many comments to read for each post
-    comments_per_post = 1
-    
-    posts, last_post_id = get_subreddit_posts(session=session, subreddit_name=target_subreddit, limit=num_posts_required)
-    
+
+    posts = get_posts(
+        session=session, subreddit_name=subreddit_name, limit=num_posts_required)
+
     for post in posts:
         post_title = post['title']
         post_url = post['url']
@@ -79,15 +90,19 @@ def main():
         print(post_title)
         print(post_url)
 
-        comments = get_comments_for_post(session=session, subreddit_name=target_subreddit, post_id=post_id, comment_count=comments_per_post)
+        comments = get_comments(
+            session=session, subreddit_name=subreddit_name, post_id=post_id, comment_count=comments_per_post)
         for comment in comments:
             print(comment['body'])
             print(comment['permalink'])
 
         print('')
 
+
 if __name__ == '__main__':
-    main()
-
-
-    
+    target_subreddit = 'AskReddit'
+    num_posts_required = 1
+    # How many comments to read for each post
+    comments_per_post = 1
+    get_subreddit_thread(subreddit_name=target_subreddit,
+                         num_posts_required=num_posts_required, comments_per_post=comments_per_post)
