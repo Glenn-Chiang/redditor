594cdc163a45cc1daed4eb9b8e4bd6d55011379d
Glenn <glennchiang24@gmail.com>
2024-03-07 13:34:17 +0800

    Modify get_subreddit_thread to return list of distilled posts with nested comments

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -9,9 +9,8 @@ REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
 REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
 API_URL = 'https://oauth.reddit.com'
 
+
 # Request for auth token and attach it to request headers
-
-
 def authenticate_session(session: requests.Session, client_id: str, client_secret: str) -> requests.Session:
     basic_auth = HTTPBasicAuth(username=client_id, password=client_secret)
     payload = {'grant_type': 'client_credentials'}
@@ -46,7 +45,7 @@ def get_posts(session: requests.Session, subreddit_name: str, limit: int, after:
         {
             'id': post['data']['id'],
             'title': post['data']['title'],
-            'url': post['data']['url']
+            'url': post['data']['url'],
         } for post in response_data['children']
     ]
     return posts
@@ -66,11 +65,11 @@ def get_comments(session: requests.Session, subreddit_name: str, post_id: str, c
             'permalink': comment['data']['permalink']
         } for comment in response.json()[1]['data']['children'][:-1]
     ]
-                
+
     return comments
 
 
-def get_subreddit_thread(subreddit_name: str, num_posts_required: int, comments_per_post: int):
+def get_subreddit_thread(subreddit_name: str, num_posts_required: int, comments_per_post: int) -> list[dict]:
     session = requests.Session()
     try:
         session = authenticate_session(
@@ -82,21 +81,13 @@ def get_subreddit_thread(subreddit_name: str, num_posts_required: int, comments_
     posts = get_posts(
         session=session, subreddit_name=subreddit_name, limit=num_posts_required)
 
+    # Add corresponding comments to each post object
     for post in posts:
-        post_title = post['title']
-        post_url = post['url']
-        post_id = post['id']
-        print(post_id)
-        print(post_title)
-        print(post_url)
-
         comments = get_comments(
-            session=session, subreddit_name=subreddit_name, post_id=post_id, comment_count=comments_per_post)
-        for comment in comments:
-            print(comment['body'])
-            print(comment['permalink'])
+            session=session, subreddit_name=subreddit_name, post_id=post['id'], comment_count=comments_per_post)
+        post['comments'] = comments
 
-        print('')
+    return posts
 
 
 if __name__ == '__main__':
@@ -104,5 +95,6 @@ if __name__ == '__main__':
     num_posts_required = 1
     # How many comments to read for each post
     comments_per_post = 1
-    get_subreddit_thread(subreddit_name=target_subreddit,
+    posts = get_subreddit_thread(subreddit_name=target_subreddit,
                          num_posts_required=num_posts_required, comments_per_post=comments_per_post)
+    print(posts)
