71c89013c7f943bbdaca71f1b847b6b9533673f6
Glenn <glennchiang24@gmail.com>
2024-03-07 13:13:02 +0800

    Extract comments from post

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
 REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
-
+API_URL = 'https://oauth.reddit.com'
 
 # Request for auth token and attach it to request headers
 def authenticate_session(session: requests.Session, client_id: str, client_secret: str) -> requests.Session:
@@ -16,7 +16,7 @@ def authenticate_session(session: requests.Session, client_id: str, client_secre
 
     try:
         response = requests.post(
-            'https://www.reddit.com/api/v1/access_token', auth=basic_auth, data=payload)
+            'https://www.reddit.com/api/v1/access_token', auth=basic_auth, data=payload, headers={'User-Agent': 'reddit-scraper:1.0'})
         response.raise_for_status()
     except RequestException:
         raise
@@ -31,19 +31,10 @@ def authenticate_session(session: requests.Session, client_id: str, client_secre
     return session
 
 
-def get_subreddit_posts(subreddit_name: str, limit: int, after: str = None) -> tuple[list, str]:
-    session = requests.Session()
-
-    try:
-        session = authenticate_session(
-            session=session, client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET)
-    except RequestException as error:
-        raise RequestException(
-            f'Error authenticating with reddit API: {error}')
-
+def get_subreddit_posts(session: requests.Session, subreddit_name: str, limit: int, after: str = None) -> tuple[list, str]:
     try:
         response = session.get(
-            f'https://oauth.reddit.com/r/{subreddit_name}/top', params={'limit': limit, 'after': after})
+            f'{API_URL}/r/{subreddit_name}/top', params={'limit': limit, 'after': after})
         response.raise_for_status()
     except RequestException as error:
         raise RequestException(f'Error getting posts from subreddit: {error}')
@@ -54,10 +45,49 @@ def get_subreddit_posts(subreddit_name: str, limit: int, after: str = None) -> t
     return posts, last_post_id
 
 
-if __name__ == '__main__':
-    posts, last_post_id = get_subreddit_posts(subreddit_name='AskReddit', limit=1)
+def get_comments_for_post(session: requests.Session, subreddit_name: str, post_id: str, comment_count: int):
+    try:
+        response = session.get(f'{API_URL}/r/{subreddit_name}/comments/{post_id}', params={'limit': comment_count})
+        response.raise_for_status()
+    except RequestException as error:
+        raise RequestException(f'Error getting comments for post: {error}')
+
+    comments = [comment['data'] for comment in response.json()[1]['data']['children']][:-1]
+    return comments
+
+def main():
+    session = requests.Session()
+    try:
+        session = authenticate_session(
+            session=session, client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET)
+    except RequestException as error:
+        raise RequestException(
+            f'Error authenticating with reddit API: {error}')
+    
+    target_subreddit = 'AskReddit'
+    num_posts_required = 1
+    # How many comments to read for each post
+    comments_per_post = 1
+    
+    posts, last_post_id = get_subreddit_posts(session=session, subreddit_name=target_subreddit, limit=num_posts_required)
+    
     for post in posts:
-        print(post['title'])
-        print(post['selftext_html'])
-        print(post['url'])
-        print(post['num_comments'])
+        post_title = post['title']
+        post_url = post['url']
+        post_id = post['id']
+        print(post_id)
+        print(post_title)
+        print(post_url)
+
+        comments = get_comments_for_post(session=session, subreddit_name=target_subreddit, post_id=post_id, comment_count=comments_per_post)
+        for comment in comments:
+            print(comment['body'])
+            print(comment['permalink'])
+
+        print('')
+
+if __name__ == '__main__':
+    main()
+
+
+    
