9ac3fa9cd6e18093213dc2cef640636e147052dd
Glenn <glennchiang24@gmail.com>
2024-03-08 21:01:05 +0800

    Add resizing logic to get_background_clip in video_maker

--- video_maker.py
+++ video_maker.py
@@ -1,8 +1,31 @@
+import random
 import os
-from moviepy.editor import AudioFileClip, VideoFileClip, ImageClip, concatenate_videoclips
+from moviepy.editor import AudioFileClip, VideoFileClip, ImageClip, CompositeVideoClip, concatenate_videoclips
+from moviepy.video.fx.crop import crop
+import PIL.Image
+
+# Extract a random clip from a video to use as background clip
+def get_background_clip(video_path: str, duration: int, size: tuple[int, int]) -> VideoFileClip:
+    source_video = VideoFileClip(video_path)
+    start_time = random.randint(
+        0, int(source_video.duration) - duration)
+    clip: VideoFileClip = source_video.subclip(
+        start_time, start_time + duration)
+    
+    original_width, original_height = clip.size
+    original_aspect_ratio = original_width / original_height
+    
+    target_width, target_height = size
+    target_aspect_ratio = target_width / target_height
+
+    cropped_width = original_width if original_aspect_ratio <= target_aspect_ratio else int(original_height * target_aspect_ratio)
+    cropped_height = original_height if original_aspect_ratio > target_aspect_ratio else int(original_width / target_aspect_ratio)
+
+    cropped_clip: VideoFileClip = crop(clip=clip, width=cropped_width, height=cropped_height, x_center=clip.size[0]//2, y_center=clip.size[1]//2)
+    return cropped_clip.resize(size, PIL.Image.LANCZOS)
 
 
-def make_video(audio_dir: str, image_dir: str, output_path: str):
+def make_video(audio_dir: str, image_dir: str, background_video_path: str, output_path: str) -> None:
     image_paths = [os.path.join(image_dir, file)
                    for file in os.listdir(image_dir)]
     # Sort image files by date created
@@ -22,13 +45,23 @@ def make_video(audio_dir: str, image_dir: str, output_path: str):
         audio_clip = AudioFileClip(filename=audio_path)
         image_clip: ImageClip = ImageClip(
             img=image_path, duration=audio_clip.duration).set_audio(audio_clip)
-        # image_clip.write_videofile(output_path, fps=24)
         image_clips.append(image_clip)
 
-    video = concatenate_videoclips(image_clips, method='compose')
-    video.write_videofile(output_path, fps=1)
+    # Video containing sequence of images
+    images_video = concatenate_videoclips(image_clips, method='compose')
+    # image_video.write_videofile(output_path, fps=1)
+
+    background_video = get_background_clip(
+        video_path=background_video_path, duration=int(images_video.duration))
+
+    composite_video = CompositeVideoClip(
+        [background_video, images_video.set_position('center')])
+    composite_video.write_videofile(output_path)
 
 
 if __name__ == '__main__':
-    make_video(audio_dir='tmp/audio', image_dir='tmp/screenshots',
-               output_path='tmp/video/test.mp4')
+    # make_video(audio_dir='tmp/audio', image_dir='tmp/screenshots',
+    #            background_video_path='assets/gameplay.mp4', output_path='tmp/video/test.mp4')
+    clip = get_background_clip(video_path='assets/gameplay.mp4',
+                        duration=5, size=(1080, 1920))
+    clip.write_videofile('output/test.mp4')
