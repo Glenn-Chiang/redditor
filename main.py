ba01c1e358867ab7e3902a004bbbc78ce9aafe5e
Glenn <glennchiang24@gmail.com>
2024-03-10 17:48:20 +0800

    Refactor script to fetch multiple posts from subreddit and allow user to select a post before fetching comments for the selected post

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from uuid import uuid4
-from reddit_scraper import get_subreddit_thread
+from reddit_client import reddit_client
 from text_to_speech import generate_audio
 from screenshot_downloader import screenshot_post, screenshot_comment
 from video_maker import make_video
@@ -14,9 +14,10 @@ background_video_path = 'assets/gameplay.mp4'
 
 video_size = (1080, 1920)  # width, height
 max_video_duration = 60  # in seconds
+
 target_subreddit = 'AskReddit'
-num_posts_required = 2
-comments_per_post = 10
+num_posts = 10 # How many posts to fetch from subreddit. User will be prompted to select a post from this list to use as the subject of the video.
+comments_per_post = 5 # How many comments to include per video
 
 
 def generate():
@@ -25,73 +26,96 @@ def generate():
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-    # Getting subreddit thread
-    print(f'Getting thread from r/{target_subreddit}...')
-    posts = get_subreddit_thread(subreddit_name=target_subreddit,
-                                 num_posts_required=num_posts_required, comments_per_post=comments_per_post)
+    # Get trending posts from subreddit
+    print(f'Getting posts from r/{target_subreddit}...')
+    try:
+        posts = reddit_client.get_posts(subreddit_name=target_subreddit, num_posts=num_posts)
+    except Exception as error:
+        print('Error getting posts:', error)
+        return
 
-    for post in posts:
-        print('Post:', post['url'])
-        print('Comments:')
-        for comment in post['comments']:
-            print(f"https://www.reddit.com{comment['permalink']}")
-        print('')
+    # Display list of trending posts and prompt user to select 1 post
+    # Repeat the prompt until user enters valid input
+    while True:
+        print(f'Select a post from below:')
+        for index, post in enumerate(posts):
+            print(f"[{index}] {post['url']}")
+        
+        user_input = input('Index number: ')
+
+        try:
+            selected_index = int(user_input)
+            post = posts[selected_index]
+            break
+        except (ValueError, IndexError):
+            print('Invalid index number\n')
+    
+    print(f"\nSelected post {post['id']}")
+    print(f"Getting comments...")
+    
+    try:
+        comments = reddit_client.get_comments(subreddit_name=target_subreddit, post_id=post['id'], num_comments=comments_per_post)
+        post['comments'] = comments
+    except Exception as error:
+        print('Error getting comments:', error)
+        return
+
+    for comment in comments:
+        print(f"https://www.reddit.com{comment['permalink']}")
 
     # Generate audio files for each post/comment
-    print('Generating audio...')
-    for post in posts:
-        # Generate audio for post title
-        filename = f"t3_{post['id']}.wav"
-
+    print('\nGenerating audio...')
+    
+    # Generate audio for post title
+    title_audio_filename = f"t3_{post['id']}.wav"
+    try:
+        generate_audio(text=post['title'], output_path=os.path.join(
+            audio_directory, title_audio_filename))
+        print('Generated audio:', title_audio_filename)
+    except Exception as error:
+        # If there was an error generating audio for post title, exit program
+        print(f'Error generating audio for post title')
+        return
+    
+    # Generate audio for comments
+    for comment in comments:
+        comment_audio_filename = f"t1_{comment['id']}.wav"
         try:
-            generate_audio(text=post['title'], output_path=os.path.join(
-                audio_directory, filename))
-            print('Generated audio:', filename)
+            generate_audio(text=comment['body'], output_path=os.path.join(
+                audio_directory, comment_audio_filename))
+            print('Generated audio:', comment_audio_filename)
         except Exception as error:
-            # If there was an error generating audio for this post, skip to next post
-            print(f"Error generating audio for post {post['id']}:", error)
+            # If there was an error generating audio for this comment, skip to next comment
+            print(f"Error generating audio for comment {comment['id']}:", error)
             continue
-
-        # Generate audio for comments
-        for comment in post['comments']:
-            filename = f"t1_{comment['id']}.wav"
-            try:
-                generate_audio(text=comment['body'], output_path=os.path.join(
-                    audio_directory, filename))
-                print('Generated audio:', filename)
-            except Exception as error:
-                # If there was an error generating audio for this comment, skip to next comment
-                print(f"Error generating audio for comment {comment['id']}:", error)
-                continue
 
     # Download screenshot of each post/comment
-    print('Downloading screenshots...')
-    for post in posts:
-        post_id = f"t3_{post['id']}"
-        try:
-            screenshot_post(subreddit=target_subreddit, post_id=post_id,
-                        output_path=os.path.join(screenshot_directory, f'{post_id}.png'))
-            print('Downloaded screenshot:', post_id)
-        except Exception as error:
-            print(f"Error downloading screenshot for post {post_id}:", error)
-            continue
+    print('\nDownloading screenshots...')
+    post_id = f"t3_{post['id']}"
+    try:
+        screenshot_post(subreddit=target_subreddit, post_id=post_id,
+                    output_path=os.path.join(screenshot_directory, f'{post_id}.png'))
+        print('Downloaded screenshot:', post_id)
+    except Exception as error:
+        print(f"Error downloading screenshot for post {post_id}:", error)
+        return
 
-        for comment in post['comments']:
-            comment_id = f"t1_{comment['id']}"
-            try:
-                screenshot_comment(subreddit=target_subreddit, post_id=post_id, comment_id=comment_id,
-                               output_path=os.path.join(screenshot_directory, f'{comment_id}.png'))
-                print('Downloaded screenshot:', comment_id)
-            except Exception as error:
-                print(f"Error downloading screenshot for comment {comment_id}:", error)
-                continue
+    for comment in comments:
+        comment_id = f"t1_{comment['id']}"
+        try:
+            screenshot_comment(subreddit=target_subreddit, post_id=post_id, comment_id=comment_id,
+                            output_path=os.path.join(screenshot_directory, f'{comment_id}.png'))
+            print('Downloaded screenshot:', comment_id)
+        except Exception as error:
+            print(f"Error downloading screenshot for comment {comment_id}:", error)
+            continue
             
     print('Creating video...')
-    output_path = os.path.join(output_directory, f'{uuid4()}.mp4')
+    output_path = os.path.join(output_directory, f"{post['id']}.mp4")
     make_video(audio_dir=audio_directory, image_dir=screenshot_directory, background_video_path=background_video_path,
                output_path=output_path, video_size=video_size, max_duration=max_video_duration)
 
-    print('Video created!')
+    print('Video generated!')
 
     # Remove temp directory TODO: Uncomment this in production
     shutil.rmtree(temp_directory)
---
+++ reddit_client.py
@@ -0,0 +1,92 @@
+import requests
+from requests.exceptions import RequestException
+from requests.auth import HTTPBasicAuth
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
+API_URL = 'https://oauth.reddit.com'
+
+
+class RedditClient():
+    session: requests.Session
+
+    def __init__(self, client_id, client_secret) -> None:
+        self.session = requests.Session()
+        self.authenticate(client_id=client_id, client_secret=client_secret)
+
+    # Request for auth token and attach it to request headers
+    def authenticate(self, client_id: str, client_secret: str) -> None:
+        basic_auth = HTTPBasicAuth(username=client_id, password=client_secret)
+        payload = {'grant_type': 'client_credentials'}
+
+        try:
+            response = requests.post(
+                'https://www.reddit.com/api/v1/access_token', auth=basic_auth, data=payload, headers={'User-Agent': 'reddit-scraper:1.0'})
+            response.raise_for_status()
+        except RequestException as error:
+            raise RequestException(f'Error authenticating with reddit API: {error}')
+
+        token = response.json()['access_token']
+
+        self.session.headers.update({
+            'Authorization': f'bearer {token}',
+            'User-Agent': 'reddit-scraper:1.0'
+        })
+
+
+    # Get given number of posts for given subreddit
+    def get_posts(self, subreddit_name: str, num_posts: int) -> list[dict]:
+        try:
+            response = self.session.get(
+                f'{API_URL}/r/{subreddit_name}/top', params={'limit': num_posts})
+            response.raise_for_status()
+        except RequestException as error:
+            raise RequestException(
+                f'Error getting posts from subreddit: {error}')
+
+        response_data = response.json()['data']
+        posts = [
+            {
+                'id': post['data']['id'],
+                'title': post['data']['title'],
+                'url': post['data']['url'],
+            } for post in response_data['children']
+        ]
+        return posts
+
+
+    # Get given number of comments for given post in subreddit
+    def get_comments(self, subreddit_name: str, post_id: str, num_comments: int) -> list[dict]:
+        comments = []
+        last_comment_id = None
+
+        # You're probably wondering why we can't just make a single batch request for the required number of comments.
+        # The truth is, I have no idea. The API just always decides to return fewer comments than what was requested.
+        while len(comments) < num_comments:
+            try:
+                response = self.session.get(
+                    f'{API_URL}/r/{subreddit_name}/comments/{post_id}', params={'after': last_comment_id})
+                response.raise_for_status()
+            except RequestException as error:
+                raise RequestException(
+                    f'Error getting comments for post: {error}')
+
+            last_comment_id = response.json()[1]['data']['after']
+
+            for comment in response.json()[1]['data']['children'][:-1]:
+                if len(comments) < num_comments:
+                    comments.append({
+                        'id': comment['data']['id'],
+                        'body': comment['data']['body'],
+                        'permalink': comment['data']['permalink']
+                    })
+                else:
+                    break
+
+        return comments
+
+
+reddit_client = RedditClient(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET)
