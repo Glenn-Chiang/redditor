5a63d251e2bee7f60f7e6c3d1a42766e993460f7
Glenn <glennchiang24@gmail.com>
2024-03-13 15:04:03 +0800

    Refactor screenshot_downloader to download both post and comments in same browser instance

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from reddit_client import reddit_client
 from text_to_speech import generate_audio
-from screenshot_downloader import screenshot_post, screenshot_comment
+from screenshot_downloader import screenshot_post, screenshot_comment, screenshot_thread
 from video_maker import make_video
 from input_prompts import select_post, select_subreddit
 
@@ -16,8 +16,10 @@ VIDEO_SIZE = (1080, 1920)  # width, height
 MAX_DURATION = 45  # in seconds
 
 subreddits = ['AskReddit', 'Showerthoughts', 'funny', 'AskMen']
-num_posts = 10 # How many posts to fetch from the selected subreddit. User will be prompted to select a post from this list.
-max_comments = 20
+# How many posts to fetch from the selected subreddit. User will be prompted to select a post from this list.
+num_posts = 10
+max_comments = 10
+
 
 def generate():
     # Create necessary directories if they have not been created
@@ -30,18 +32,20 @@ def generate():
     # Get trending posts from subreddit
     print(f'Getting posts from r/{target_subreddit}...')
     try:
-        posts = reddit_client.get_posts(subreddit_name=target_subreddit, num_posts=num_posts)
+        posts = reddit_client.get_posts(
+            subreddit_name=target_subreddit, num_posts=num_posts)
     except Exception as error:
         print('Error getting posts:', error)
         return
 
     post = select_post(posts)
-    
+
     print(f"\nSelected post {post['id']}: {post['title']}")
     print(f"Getting comments...")
-    
+
     try:
-        comments = reddit_client.get_comments(subreddit_name=target_subreddit, post_id=post['id'], num_comments=max_comments)
+        comments = reddit_client.get_comments(
+            subreddit_name=target_subreddit, post_id=post['id'], num_comments=max_comments)
         post['comments'] = comments
     except Exception as error:
         print('Error getting comments:', error)
@@ -52,7 +56,7 @@ def generate():
 
     # Generate audio files for each post/comment
     print('\nGenerating audio...')
-    
+
     # Generate audio for post title
     title_audio_filename = f"t3_{post['id']}.wav"
     try:
@@ -63,7 +67,7 @@ def generate():
         # If there was an error generating audio for post title, exit program
         print('Error generating audio for post:', error)
         return
-    
+
     # Generate audio for comments
     for comment in comments:
         comment_audio_filename = f"t1_{comment['id']}.wav"
@@ -73,30 +77,17 @@ def generate():
             print('Generated audio:', comment_audio_filename)
         except Exception as error:
             # If there was an error generating audio for this comment, skip to next comment
-            print(f"Error generating audio for comment {comment['id']}:", error)
+            print(
+                f"Error generating audio for comment {comment['id']}:", error)
             continue
 
-    # Download screenshot of each post/comment
+    # TODO: Remove audio files of comments that would exceed video duration. Then, only download screenshots for remaining comments.
+
     print('\nDownloading screenshots...')
-    post_id = f"t3_{post['id']}"
-    try:
-        screenshot_post(subreddit=target_subreddit, post_id=post_id,
-                    output_path=os.path.join(screenshot_directory, f'{post_id}.png'))
-        print('Downloaded screenshot:', post_id)
-    except Exception as error:
-        print(f"Error downloading screenshot for post:", error)
-        return
 
-    for comment in comments:
-        comment_id = f"t1_{comment['id']}"
-        try:
-            screenshot_comment(subreddit=target_subreddit, post_id=post_id, comment_id=comment_id,
-                            output_path=os.path.join(screenshot_directory, f'{comment_id}.png'))
-            print('Downloaded screenshot:', comment_id)
-        except Exception as error:
-            print(f"Error downloading screenshot for comment {comment_id}:", error)
-            continue
-            
+    screenshot_thread(subreddit=target_subreddit, post_id=f"t3_{post['id']}", comment_ids=[
+                      f"t1_{comment['id']}" for comment in comments], output_dir=screenshot_directory)
+
     print('Creating video...')
     video_filename = post['url'].split('/')[7] + '.mp4'
     output_path = os.path.join(output_directory, video_filename)
--- screenshot_downloader.py
+++ screenshot_downloader.py
@@ -1,6 +1,12 @@
+import time
 from playwright.sync_api import sync_playwright
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81'
+REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
+REDDIT_PASSWORD = os.getenv('REDDIT_PASSWORD')
 
 
 def screenshot_post(subreddit: str, post_id: str, output_path: str):
@@ -15,6 +21,7 @@ def screenshot_post(subreddit: str, post_id: str, output_path: str):
 
 # Handle nsfw
 
+
 def screenshot_comment(subreddit: str, post_id: str, comment_id: str, output_path: str):
     with sync_playwright() as playwright:
         browser = playwright.webkit.launch()
@@ -22,16 +29,90 @@ def screenshot_comment(subreddit: str, post_id: str, comment_id: str, output_pat
         page = browser.new_page(user_agent=USER_AGENT)
         page.goto(
             url=f'https://www.reddit.com/r/{subreddit}/comments/{post_id}/comment/{comment_id}')
-        
-        comment_element = page.locator(f"shreddit-comment[thingid='{comment_id}']")
+
+        comment_element = page.locator(
+            f"shreddit-comment[thingid='{comment_id}']")
         # Button that toggles whether to expand or close the comment subtree
-        toggle_button = comment_element.get_by_label('Toggle Comment Thread').first
+        toggle_button = comment_element.get_by_label(
+            'Toggle Comment Thread').first
         # If expanded, click on toggle button to collapse the subtree
         if toggle_button.get_attribute('aria-expanded'):
             toggle_button.click()
-        
+
         comment_element.screenshot(path=output_path)
         browser.close()
 
+
+# Download screenshot of post and comments while launching 1 browser
+def screenshot_thread(subreddit: str, post_id: str, comment_ids: list[str], output_dir: str, nsfw: bool = False):
+    with sync_playwright() as playwright:
+        browser = playwright.webkit.launch(headless=False)
+
+        # Change user-agent to avoid being detected as a bot
+        page = browser.new_page(user_agent=USER_AGENT)
+        page.goto(
+            url=f'https://www.reddit.com/r/{subreddit}/comments/{post_id}')
+
+        # If post is marked as nsfw, we need to login
+        if nsfw:
+            page.locator('#login-button').click()
+            page.locator('#login-password').first.fill(REDDIT_PASSWORD)
+            page.locator('#login-username').first.fill(REDDIT_USERNAME)
+            page.locator('#login > faceplate-tabpanel > auth-flow-modal:nth-child(1) > div.w-100 > faceplate-tracker > button').click()
+        
+        # Screenshot post
+        page.locator(f'#{post_id}').screenshot(
+            path=os.path.join(output_dir, f'{post_id}.png'))
+        print('Downloaded screenshot for post title')
+
+        # Screenshot comments
+        for comment_id in comment_ids:
+            try:
+                page.goto(
+                    url=f'https://www.reddit.com/r/{subreddit}/comments/{post_id}/comment/{comment_id}')
+                comment_element = page.locator(
+                    f"shreddit-comment[thingid='{comment_id}']")
+                # Button that toggles expansion of comment subtree
+                toggle_button = comment_element.get_by_label(
+                    'Toggle Comment Thread').first
+                # If subtree is expanded, click on toggle button to collapse the subtree
+                if toggle_button.get_attribute('aria-expanded'):
+                    toggle_button.click()
+
+                comment_element.screenshot(
+                    path=os.path.join(output_dir, f'{comment_id}.png'))
+                print(f'Downloaded screenshot for comment {comment_id}')
+
+            except Exception as error:
+                print(
+                    f"Error downloading screenshot for comment {comment_id}:", error)
+                continue
+
+        browser.close()
+
+
+def testA():
+    start = time.time()
+
+    post_id = 't3_1bcsmoy'
+    comment_ids = [
+        't1_kujdv7e',
+        't1_kui0m13',
+        't1_kuiahqp',
+        't1_kuk2ag6',
+        't1_kui9po2',
+        't1_kujdv7e',
+        't1_kui0m13',
+        't1_kuiahqp',
+        't1_kuk2ag6',
+        't1_kui9po2',
+    ]
+    screenshot_thread(subreddit='AskReddit', post_id=post_id,
+                      comment_ids=comment_ids, output_dir='tmp/screenshots', nsfw=True)
+
+    end = time.time()
+    print(f'Finished in {round(end - start, 2)}s')
+
+
 if __name__ == '__main__':
-    screenshot_comment(subreddit='AskReddit', post_id='t3_1b8sfer', comment_id='t1_ktr4wrw', output_path='output/test.png')
+    testA()
